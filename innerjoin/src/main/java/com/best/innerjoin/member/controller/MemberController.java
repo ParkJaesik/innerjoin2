dad89f3ba75a30a6af398fee5ef5be3bf76979a4
user1 <user1@user1-PC>
2019-09-26 12:11:00 +0900

    회원가입 기능(암호화X)

--- innerjoin/src/main/java/com/best/innerjoin/member/controller/MemberController.java
+++ innerjoin/src/main/java/com/best/innerjoin/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.best.innerjoin.member.model.service.MemberService;
 import com.best.innerjoin.member.model.vo.Member;
@@ -14,7 +15,13 @@ public class MemberController {
 	@Autowired
 	private MemberService mService;
 	
+	// 로그인 폼으로 가기
+	@RequestMapping("loginForm.ij")
+	public String loginMember() {
+		return "member/loginForm";
+	}
 	
+	// 로그인
 	@RequestMapping("login.ij")
 	public String loginMember(Member member,Model model) {
 		
@@ -26,13 +33,28 @@ public class MemberController {
 		
 	}
 	
-	@RequestMapping("loginForm.ij")
-	public String loginMember() {
-		return "member/loginForm";
-	}
 	
+	// 회원가입 폼으로 가기
 	@RequestMapping("joinForm.ij")
 	public String joinMember() {
 		return "member/joinForm";
 	}
+	
+	// 회원가입
+	@RequestMapping(value="join.ij", method=RequestMethod.POST)
+	public String insertMember(Member member, Model model) {
+		System.out.println(member);
+		
+		int result = mService.insertMember(member);
+		
+		
+		if(result>0) {
+			return "redirect:loginForm.ij";
+		}else {
+			model.addAttribute("msg", "회원 가입 실패");
+			return "common/errorPage";
+		}
+	}
+	
+
 }
--- innerjoin/src/main/java/com/best/innerjoin/member/model/service/MemberServiceImpl.java
+++ innerjoin/src/main/java/com/best/innerjoin/member/model/service/MemberServiceImpl.java
@@ -1,7 +1,11 @@
 package com.best.innerjoin.member.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.best.innerjoin.member.model.dao.MemberDao;
 import com.best.innerjoin.member.model.vo.Member;
@@ -11,10 +15,25 @@ public class MemberServiceImpl implements MemberService{
 	
 	@Autowired
 	private MemberDao mDao;
+	
+	
+//  암호화	
+//	@Autowired
+//	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public Member loginMember(Member member) {
 		
 		return mDao.loginMember(member);
+	}
+	
+	@Override
+//	@Transactional(propagation=Propagation.REQUIRED, isolation=Isolation.READ_COMMITTED, rollbackFor=Exception.class)
+	public int insertMember(Member member) {
+//		String encPwd = bCryptPasswordEncoder.encode(member.getMemberPwd());
+//		
+//		member.setMemberPwd(encPwd);
+		
+		return mDao.insertMember(member);
 	}
 }
