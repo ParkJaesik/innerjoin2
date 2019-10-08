363f7e0669fdddc825731eaf3e5badf27938a4be
user1 <user1@user1-PC>
2019-10-08 14:08:27 +0900

    admin 임시

---
+++ innerjoin/src/main/java/com/best/innerjoin/admin/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.best.innerjoin.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.best.innerjoin.admin.model.service.AdminService;
+
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private AdminService adService;
+	
+	//임시 관리자 페이지로 이동
+	@RequestMapping("admin.ij")
+	public String adminView() {
+		return "admin/admin";
+	}
+
+	/** 회원관리페이지 이동
+	 * @return
+	 */
+	@RequestMapping("manageMember.ij")
+	public String manageMemberView() {
+		return "admin/manageMember";
+	}
+	
+	
+
+
+}
