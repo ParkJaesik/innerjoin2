16bace597198b940ac7bbc6fa73c5837bb5a8049
user1 <user1@user1-PC>
2019-10-15 10:52:28 +0900

    모임 검색, 유저 검색 (검색만)

--- innerjoin/src/main/java/com/best/innerjoin/mgSearch/model/service/MgSearchServiceImpl.java
+++ innerjoin/src/main/java/com/best/innerjoin/mgSearch/model/service/MgSearchServiceImpl.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
+import com.best.innerjoin.group.model.vo.Group;
 import com.best.innerjoin.member.model.vo.Member;
 import com.best.innerjoin.mgSearch.model.dao.MgSearchDao;
+import com.best.innerjoin.mgSearch.model.vo.MgSearch;
 import com.best.innerjoin.mgSearch.model.vo.PageInfo;
 import com.best.innerjoin.mgSearch.model.vo.Pagination;
 
@@ -20,10 +20,40 @@ public class MgSearchServiceImpl implements MgSearchService{
 
 	// 유저 검색페이지 첫 화면
 	@Override
-	public ArrayList<Member> selectList(int currentPage) {
-		int listCount= mgDao.getListCount();
+	public ArrayList<Member> selectMList(int currentPage) {
+		int listCount= mgDao.getMListCount();
 		
 		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
-		return mgDao.selectList(pi);
+		return mgDao.selectMList(pi);
 	}
+
+	// 모임검색  첫 화면
+	@Override
+	public ArrayList<Group> selectGList(int currentPage) {
+		int listCount = mgDao.getGListCount();
+		
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
+		return mgDao.selectGList(pi);
+	}
+
+	// 모임 조건 검색
+	@Override
+	public ArrayList<Group> selectGsList(int currentPage, MgSearch mg) {
+		int listCount = mgDao.getGsListCount(mg);
+		
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
+
+		return mgDao.selectGsList(pi,mg);
+	}
+
+	// 유저 상세 검색
+	@Override
+	public ArrayList<Member> selectMsList(int currentPage, MgSearch mg) {
+		int listCount = mgDao.getMsListCount(mg);
+		
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount);
+		
+		return mgDao.selectMsList(pi,mg);
+	}
+
 }
